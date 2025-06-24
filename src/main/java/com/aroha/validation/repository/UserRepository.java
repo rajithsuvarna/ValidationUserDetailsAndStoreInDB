1bb0e28cfaa07fa4b0866e1974d2b32f69d734b5
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2025-06-24 11:49:59 +0530

    Update UserRepository.java

--- src/main/java/com/aroha/validation/repository/UserRepository.java
+++ src/main/java/com/aroha/validation/repository/UserRepository.java
@@ -16,7 +16,7 @@ public interface UserRepository extends JpaRepository<User,UserId>{
 	//Native query to insert each record
 	@Modifying
 	@Transactional
-	@Query(value="INSERT INTO user(f_name,l_name,gender,date_of_birth,record_no,education,house_number,address1,address2,city,pincode,mobile_number,company,monthly_salary) VALUES(:lName,:lName,:gender,:dob,:recordNo,:education,:houseNumber,:address1,:address2,:city,:pincode,:mobileNumber,:company,:monthlySalary)",nativeQuery=true)
+	@Query(value="INSERT INTO user(f_name,l_name,gender,date_of_birth,record_no,education,house_number,address1,address2,city,pincode,mobile_number,company,monthly_salary) VALUES(:fName,:lName,:gender,:dob,:recordNo,:education,:houseNumber,:address1,:address2,:city,:pincode,:mobileNumber,:company,:monthlySalary)",nativeQuery=true)
 	void insertUser(@Param("fName") String fName,@Param("lName") String lName,@Param("gender") String gender, @Param("dob") String dateOfBirth, @Param("recordNo") int recordNo,@Param("education") String education,
 			@Param("houseNumber") int houseNumber,@Param("address1") String address1,@Param("address2")  String address2,@Param("city")  String city, @Param("pincode") int pincode,@Param("mobileNumber")  String mobileNumber,
 			@Param("company") String company,@Param("monthlySalary")  double monthlySalary);
