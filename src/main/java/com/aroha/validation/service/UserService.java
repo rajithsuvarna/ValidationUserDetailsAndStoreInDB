b9ec2737a0e15e85da308be8677afeb1219bde87
Rajith Suvarna D <109891044+rajithsuvarna@users.noreply.github.com>
2025-06-25 09:57:02 +0530

    Update UserService.java
    Updated service layer

--- src/main/java/com/aroha/validation/service/UserService.java
+++ src/main/java/com/aroha/validation/service/UserService.java
@@ -1,11 +1,22 @@
 package com.aroha.validation.service;
 
+import java.util.List;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import com.aroha.validation.dto.UserResponse;
+import com.aroha.validation.dto.UserSalaryProjection;
+import com.aroha.validation.dto.UserSummaryDTO;
+import com.aroha.validation.entity.User;
 
 public interface UserService {
 
 	UserResponse getValidated(MultipartFile file);
+
+	List<UserSummaryDTO> getFilteredUsers(String firstname, String lastname);
+
+	List<User> findUsersByDobBetween(String startDate, String endDate);
+
+	List<UserSalaryProjection> findBySalary(double salary);
 
 }
